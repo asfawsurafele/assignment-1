3a3a0d139a3a699c02baf088fbaa6365d8b5c659
Surafele Asfaw <surafeleasfaw@Surafeles-MacBook-Air.local>
2022-11-29 12:24:46 -0800

    darktheme function finished

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,4 +1,4 @@
-onst mynotes = document.querySelector('.mynotes');
+const mynotes = document.querySelector('.mynotes');
 const art = document.querySelector('.container');
 const footer = document.querySelector('.bottomcontainer');
 const keepintouch = document.querySelector('.bottomtext');
@@ -20,3 +20,29 @@ let notesArray = [{
     title: "note one",
     body: "this is my first note"
 }];
+
+function darktheme1(){
+    txtbox.classList.toggle('textareabackground');
+    txtarea.classList.toggle('ta::-webkit-input-placeholder');
+    txtarea.classList.toggle('textwhite')
+    aboutdocs[0].classList.toggle('aboutdocs1');
+    aboutdocs[1].classList.toggle('aboutdocs1');
+    cancelbutton.classList.toggle('redbutton');
+    newnotebutt.classList.toggle('bluebutton');
+    savebutton.classList.toggle('bluebutton');
+    art.classList.toggle('darkthememode2');
+    footer.classList.toggle('darkthememode2');
+    body.classList.toggle('darkthememode2');
+    takebetternotes.classList.toggle('textwhite');
+    keepintouch.classList.toggle('textwhite');
+    if (drkbutton.textContent == 'Light Theme'){
+        drkbutton.textContent = 'Dark Theme'
+    }
+    else{
+        drkbutton.textContent = 'Light Theme'
+    };
+    drkbutton.classList.toggle('darktolight');
+    mynotes.classList.toggle('darkthememode');
+    mynotestxt1.classList.toggle('textwhite');
+    mynotestxt2.classList.toggle('textwhite');
+}
